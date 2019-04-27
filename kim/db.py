65d971c03700e53a81a3e5fb219b933e52c41c78
hyesung <khs990419@gmail.com>
2019-04-28 06:22:59 +0900

    additional 3 questions

--- kim/db.py
+++ kim/db.py
@@ -7,11 +7,11 @@ conn = pymysql.connect(host = 'localhost',
                        charset = 'utf8')
 
 
-def choice(phase, num, gender, age, depart):
+def choice(phase, num, gender, age, depart, depart_it, last_name, jeong_in_name):
     try:
         with conn.cursor() as cursor:
-            sql = 'SELECT * FROM aki WHERE phase = %s AND num = %s AND gender = %s AND age = %s AND depart = %s'
-            cursor.execute(sql, [phase, num, gender, age, depart])
+            sql = 'SELECT NAME FROM aki WHERE phase = %s AND num = %s AND gender = %s AND age = %s AND depart = %s AND depart_it = %s AND last_name = %s AND jeong_in_name = %s'
+            cursor.execute(sql, [phase, num, gender, age, depart, depart_it, last_name, jeong_in_name])
             row = cursor.fetchall()
             return row
     finally:
--- kim/server.py
+++ kim/server.py
@@ -3,12 +3,6 @@ import pymysql, db
 
 app = Flask(__name__)
 
-phase = 0
-num = 0
-gender = 0
-age = 0
-depart = 0
-
 
 @app.route('/')
 @app.route('/question1')
@@ -18,34 +12,63 @@ def question1():
 
 @app.route('/question2', methods=['POST'])
 def question2():
+    global phase
     phase = request.form["bool"]
     return render_template('grade.html')
 
 
 @app.route('/question3', methods=['POST'])
 def question3():
+    global num
     num = request.form["bool"]
     return render_template('sex.html')
 
 
 @app.route('/question4', methods=['POST'])
 def question4():
+    global gender
     gender = request.form["bool"]
     return render_template('age.html')
 
 
 @app.route('/question5', methods=['POST'])
 def question5():
+    global age
     age = request.form["bool"]
     return render_template('major.html')
 
 
+@app.route('/question6', methods=['POST'])
+def question6():
+    global depart
+    depart = request.form["bool"]
+    return render_template('major_it.html')
+
+
+@app.route('/question7', methods=['POST'])
+def question7():
+    global depart_it
+    depart_it = request.form["bool"]
+    return render_template('last_name.html')
+
+
+@app.route('/question8', methods=['POST'])
+def question8():
+    global last_name
+    last_name = request.form["bool"]
+    return render_template('jeong.html')
+
+
 @app.route('/result', methods=['POST'])
 def result():
-    depart = request.form["bool"]
-    answer = db.choice(phase, num, gender, age, depart)
-    print(answer)  # 프론트로 반환
-    return render_template('result.html')
+    global jeong_in_name
+    jeong_in_name = request.form["bool"]
+    answer = db.choice(phase, num, gender, age, depart, depart_it, last_name, jeong_in_name)
+    if len(answer) == 0:
+        return render_template('result.html') #찾기 실패
+    else:
+        print(answer)
+    return render_template('result.html', data=answer)
 
 
 if __name__ == '__main__':
