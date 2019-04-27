a9f87ab62608b5b1847fc228b967430e17dd5328
taedong <kwanay1010@naver.com>
2019-04-27 23:37:34 +0900

    왑키네이터
    -frame 삭제
    - javascript 문법 js 파일로
    - Question 매개변수 2개
    - grade.html 수정

---
+++ javascript.js
@@ -0,0 +1,12 @@
+    var boolean;
+    var index;
+    var booldata = new Array();
+
+    function Question(bool,index){
+        if(bool==='yes'){
+          boolean = 1;
+        } else{
+          boolean = 0;
+      }
+      booldata[index] = boolean;
+    }
