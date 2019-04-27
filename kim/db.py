6697ce6f9fc8aa26f632ee4ed1fd2b007e2efc85
hyesung <khs990419@gmail.com>
2019-04-28 02:50:46 +0900

    static, templates folder

--- kim/db.py
+++ kim/db.py
@@ -6,12 +6,14 @@ conn = pymysql.connect(host = 'localhost',
                        db = 'who_r_u',
                        charset = 'utf8')
 
-def select():
+
+def choice(phase, num, gender, age, depart):
     try:
         with conn.cursor() as cursor:
-            sql = 'SELECT * FROM quiz WHERE prob_id = %d'
-            cursor.execute(sql)
-            row = cursor.fetchone()
+            sql = 'SELECT * FROM aki WHERE phase = %s AND num = %s AND gender = %s AND age = %s AND depart = %s'
+            cursor.execute(sql, [phase, num, gender, age, depart])
+            row = cursor.fetchall()
             return row
     finally:
         conn.close()
+
--- kim/server.py
+++ kim/server.py
@@ -1,7 +1,45 @@
-from flask import Flask
+from flask import Flask, render_template, request
 import pymysql, db
 
+app = Flask(__name__)
 
+phase = 0
+num = 0
+gender = 0
+age = 0
+depart = 0
+
+@app.route('/')
+@app.route('/question1')
+def question1():
+    return render_template('phase.html')
+
+
+@app.route('/question2', methods=['POST'])
+def question2():
+    phase = request.form["phase"]
+    return render_template('grade.html')
+
+
+@app.route('/question3', methods=['POST'])
+def question3():
+    return render_template('sex.html')
+
+
+@app.route('/question4', methods=['POST'])
+def question4():
+    return render_template('age.html')
+
+
+@app.route('/question5', methods=['POST'])
+def question5():
+    answer = db.choice(phase, num, gender, age, depart)
+    print(answer) # 프론트로 반환
+    return render_template('major.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=80)
 
 
 
